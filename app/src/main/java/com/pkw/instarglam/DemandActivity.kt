d5a39d97775d1dac36681dd6269fb792d38f6957
gilbertpark <gilbertpark1019@gmail.com>
2022-09-17 15:45:47 +0900

    recyclerview

--- app/src/main/java/com/pkw/instarglam/DemandActivity.kt
+++ app/src/main/java/com/pkw/instarglam/DemandActivity.kt
@@ -2,10 +2,65 @@ package com.pkw.instarglam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.pkw.instarglam.databinding.ActivityDemandBinding
 
 class DemandActivity : AppCompatActivity() {
+    lateinit var binding:ActivityDemandBinding
+    var adapter = DemandAdapter()
+    var demendDatas:ArrayList<DemandData> = arrayListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_demand)
+        binding = ActivityDemandBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setData()
+        setRv()
+    }
+
+    private fun setData() {
+        demendDatas.add(
+            DemandData(
+                "",
+                "Hong",
+                "길동"
+            )
+        )
+        demendDatas.add(
+            DemandData(
+                "",
+                "Go",
+                "길동"
+            )
+        )
+        demendDatas.add(
+            DemandData(
+                "",
+                "Dul",
+                "리"
+            )
+        )
+        demendDatas.add(
+            DemandData(
+                "",
+                "Dou",
+                "너"
+            )
+        )
+        demendDatas.add(
+            DemandData(
+                "",
+                "TTO",
+                "치"
+            )
+        )
+
+        adapter.setData(demendDatas)
+    }
+
+    private fun setRv() {
+        binding.rvDemand.layoutManager = LinearLayoutManager(this)
+        binding.rvDemand.adapter = adapter
     }
 }
--- app/src/main/java/com/pkw/instarglam/DemandAdapter.kt
+++ app/src/main/java/com/pkw/instarglam/DemandAdapter.kt
@@ -6,16 +6,38 @@ import android.view.ViewGroup
 import com.pkw.instarglam.databinding.ItemDemandBinding
 
 class DemandAdapter : RecyclerView.Adapter<DemandAdapter.ViewHolder>() {
+
+    val datas:ArrayList<DemandData> = arrayListOf()
+    fun setData(arrayList: ArrayList<DemandData>) {
+        datas.clear()
+        datas.addAll(arrayList)
+        notifyItemRangeInserted(0, datas.size)
+    }
+    fun removeData(index:Int) {
+        datas.removeAt(index)
+        notifyItemRemoved(index)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding:ItemDemandBinding =
             ItemDemandBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {}
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(datas[position])
+    }
     override fun getItemCount(): Int {
-        return 0
+        return datas.size
     }
 
-    inner class ViewHolder(val binding: ItemDemandBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class ViewHolder(val binding: ItemDemandBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: DemandData) {
+            binding.tvName.text = data.name
+            binding.tvNickName.text = data.nickname
+            binding.ivClose.setOnClickListener {
+                removeData(adapterPosition)
+            }
+        }
+    }
 }
